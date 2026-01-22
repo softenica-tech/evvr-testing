7dae621bc0658e22a4f260106b8dce9ecd8721d9
softenica-tech <softenica.cs@gmail.com>
2026-01-22 16:02:12 +0500

    video added for mobile for grey flow

--- app/home/ethos/Ethos.js
+++ app/home/ethos/Ethos.js
@@ -513,8 +513,10 @@ export default function Ethos() {
           playsInline
           preload="auto"
         >
-          {/* <source src="/assets/videos/grey-flower.mp4" type="video/mp4" />  */}
-          <source src="https://streamable.com/l/an62gf/mp4.mp4" type="video/mp4" fetchPriority="high" />
+          {/* <source src="/assets/videos/grey-flower.mp4" type="video/mp4" />  */} 
+          <source src="https://evvr-development.vercel.app/assets/videos/flower-mob.mp4" type="video/mp4" media="(max-width: 576px)" fetchPriority="high" />
+          <source src="https://streamable.com/l/an62gf/mp4.mp4" type="video/mp4" media="(min-width: 577px)" fetchPriority="high" />
+          
         </video>
       </div>
       <div className="ethos_wrapper_height"></div>
